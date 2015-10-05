0012a297dd52d66eebe66a27dd3a7e868780f3c8
Adam Sanderson <lazydayed@gmail.com>
2015-10-05 08:18:15 +0100

    Add Jimp for image processing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 
-hexo.extend.filter.register('after_post_render', require('./lib/filter'));
+// hexo.extend.filter.register('after_post_render', require('./lib/filter'));
 hexo.extend.filter.register('after_generate', require('./lib/generate-images'));
